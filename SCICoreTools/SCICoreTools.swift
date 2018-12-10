ca23291541e423879aafa73754a92a86b1e9fb10
Maxim Schleicher <msc@scireum.de>
2018-12-10 17:20:29 +0100

    Bugfix: loadingControllerFromStoryboard

--- SCICoreTools/SCICoreTools.swift
+++ SCICoreTools/SCICoreTools.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+/**
+ Loading View Controller with Identifier from named Storyboard
+ - Returns: UIViewController?
+ */
+public func loadControllerFromStoryboard(name: String, withIdentifier identifier: String, bundle: Bundle? = nil) -> UIViewController? {
+    let storyboard = UIStoryboard(name: name, bundle: bundle)
+    return storyboard.instantiateViewController(withIdentifier: identifier)
+}
+
 public struct SCICoreTools {
     /**
     Check if device is iPad.
@@ -15,15 +24,6 @@ public struct SCICoreTools {
     */
     public static var isPhone: Bool {
         return UIDevice.current.userInterfaceIdiom == .phone
-    }
-
-    /**
-     Loading View Controller with Identifier from named Storyboard
-     - Returns: UIViewController?
-     */
-    public func loadControllerFromStoryboard(name: String, withIdentifier identifier: String, bundle: Bundle? = nil) -> UIViewController? {
-        let storyboard = UIStoryboard(name: name, bundle: bundle)
-        return storyboard.instantiateViewController(withIdentifier: identifier)
     }
 }
 
