f248d5dda8b0ed6b5e2cc87f4afcf8bc9ab7d7cf
Jakob Vogel <jvo@scireum.de>
2021-12-09 00:21:06 +0100

    Outputs raw value ✏️

--- SCICoreToolsExample/ViewController.swift
+++ SCICoreToolsExample/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
 
         print("******")
         print("\(SCICoreTools.appName!) native \(SCICoreTools.deviceModel)".chainWithDashes())
-        print(UIDevice.current.modelName)
+        print(UIDevice.current.modelName.rawValue)
         self.navigationController?.pushViewController(self, animated: true, completion: {
             print("test")
         })
